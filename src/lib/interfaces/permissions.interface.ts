65cbeb63b7b0032a844a489eb0b9ea12b692acd0
Luis Aguilar <insomniocode@gmail.com>
2019-10-09 15:47:41 -0500

    [fix][interface][permissions] optional all options

--- src/lib/interfaces/permissions.interface.ts
+++ src/lib/interfaces/permissions.interface.ts
@@ -2,11 +2,11 @@
  * Defines the permission options 
  */
 export interface IPermissions {
-    readonly printing: 'highResolution' | 'lowResolution';
-    readonly modifying: boolean;
-    readonly copying: boolean;
-    readonly annotating: boolean;
-    readonly fillingForms: boolean;
-    readonly contentAccessibility: boolean;
-    readonly documentAssembly: boolean;
+    readonly printing?: 'highResolution' | 'lowResolution';
+    readonly modifying?: boolean;
+    readonly copying?: boolean;
+    readonly annotating?: boolean;
+    readonly fillingForms?: boolean;
+    readonly contentAccessibility?: boolean;
+    readonly documentAssembly?: boolean;
 }
